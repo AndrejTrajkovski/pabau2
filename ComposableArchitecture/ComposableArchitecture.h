725246f2b5ecb5340d8b4dc35ffb46e5cd1eaad8
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-01-23 16:10:15 +0700

    initial commit. Adding ComposableArchitecture

---
+++ ComposableArchitecture/ComposableArchitecture.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ComposableArchitecture.
+FOUNDATION_EXPORT double ComposableArchitectureVersionNumber;
+
+//! Project version string for ComposableArchitecture.
+FOUNDATION_EXPORT const unsigned char ComposableArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ComposableArchitecture/PublicHeader.h>
+
+
