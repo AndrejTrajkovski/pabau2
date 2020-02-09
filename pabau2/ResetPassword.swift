5a8a5d443b76bcb2e08b4b54002a33087fe8e704
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-02-09 14:13:34 +0800

    fixing reset pass screen

--- pabau2/ResetPassword.swift
+++ pabau2/ResetPassword.swift
@@ -28,35 +28,35 @@ struct ResetPassword: View {
 
 	var body: some View {
 		VStack {
-		VStack(alignment: .leading, spacing: 25) {
-			VStack(alignment: .leading, spacing: 36) {
-				Text(Texts.forgotPass)
-					.foregroundColor(.blackTwo)
-					.font(.largeTitle)
-					.frame(width: 157)
-				Text(Texts.forgotPassDescription)
-					.foregroundColor(.grey155)
-					.font(.paragraph)
-				TextAndTextView(title: Texts.resetCode.uppercased(), value: $code)
-				TextAndTextView(title: Texts.newPass.uppercased(), value: $code)
-				TextAndTextView(title: Texts.confirmPass.uppercased(), value: $code)
-			}.frame(maxWidth: 319)
-			BigButton(text: Texts.changePass) {
-				self.store.send(.changePassTapped)
+			VStack(alignment: .leading, spacing: 25) {
+				VStack(alignment: .leading, spacing: 36) {
+					Text(Texts.forgotPass)
+						.foregroundColor(.blackTwo)
+						.font(.largeTitle)
+						.frame(width: 157)
+					Text(Texts.forgotPassDescription)
+						.foregroundColor(.grey155)
+						.font(.paragraph)
+					TextAndTextView(title: Texts.resetCode.uppercased(), value: $code)
+					TextAndTextView(title: Texts.newPass.uppercased(), value: $newPass)
+					TextAndTextView(title: Texts.confirmPass.uppercased(), value: $confirmPass)
+				}.frame(maxWidth: 319)
+				BigButton(text: Texts.changePass) {
+					self.store.send(.changePassTapped)
+				}
 			}
-		}
-		.frame(minWidth: 280, maxWidth: 495)
-		.fixedSize(horizontal: false, vertical: true)
-		.navigationBarBackButtonHidden(true)
-		.navigationBarItems(leading:
-			Button(action: {
-				self.store.send(.backBtnTapped)
-			}, label: {
-				Image(systemName: "chevron.left")
-					.font(Font.title.weight(.semibold))
-				Text("Back")
-			})
-		)
+			.frame(minWidth: 280, maxWidth: 495)
+			.fixedSize(horizontal: false, vertical: true)
+			.navigationBarBackButtonHidden(true)
+			.navigationBarItems(leading:
+				Button(action: {
+					self.store.send(.backBtnTapped)
+				}, label: {
+					Image(systemName: "chevron.left")
+						.font(Font.title.weight(.semibold))
+					Text("Back")
+				})
+			)
 			Spacer()
 		}
 	}
