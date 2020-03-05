8121c7258a273aa09bf1ff1772235b82508a4e94
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-03-05 20:44:50 +0700

    fixing warnings

--- pabau2/src/FSCalendarSwiftUI/SwiftUICalendarViewModel.swift
+++ pabau2/src/FSCalendarSwiftUI/SwiftUICalendarViewModel.swift
@@ -3,11 +3,9 @@ import Combine
 import FSCalendar
 
 public class MyCalendarViewModel: ObservableObject {
-
 	@Published var scope: FSCalendarScope
 	@Published var date: Date
 	var subscriptions = Set<AnyCancellable>()
-
 	public init(_ scope: FSCalendarScope = .week,
 							_ date: Date = Date()) {
 		self.scope = scope
