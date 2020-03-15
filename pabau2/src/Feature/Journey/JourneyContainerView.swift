f67faaa49b39ef8acc134768e4065ef657ad8886
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-03-15 21:13:49 +0700

    fixing filtering

--- pabau2/src/Feature/Journey/JourneyContainerView.swift
+++ pabau2/src/Feature/Journey/JourneyContainerView.swift
@@ -4,6 +4,7 @@ import Model
 import Util
 import NonEmpty
 import ComposableArchitecture
+import SwiftDate
 
 public typealias JourneyEnvironemnt = (apiClient: JourneyAPI, userDefaults: UserDefaults)
 
@@ -36,18 +37,6 @@ public func journeyReducer(state: inout JourneyState, action: JourneyAction, env
 	return []
 }
 public struct JourneyContainerView: View {
-	let journeys: [Journey] = [
-		Journey.init(id: 0,
-								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date(), to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#9400D3"))),
-								 patient: BaseClient.init(id: 0, firstName: "Andrej", lastName: "Trajkovski", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "Bojan Trajkovskiiii"), forms: [], photos: [], postCare: [], paid: "Not Paid"),
-		Journey.init(id: 1,
-								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date(), to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Not Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#ec75ff"))),
-								 patient: BaseClient.init(id: 1, firstName: "Elon", lastName: "Musk", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "John Doe"), forms: [], photos: [], postCare: [], paid: "Paid"),
-		Journey.init(id: 2,
-								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date(), to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Not Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#88fa69"))),
-								 patient: BaseClient.init(id: 2, firstName: "Joe", lastName: "Rogan", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "Tiger Woods"), forms: [], photos: [], postCare: [], paid: "Owes 1.000")
-	]
-	
 	@State private var calendarHeight: CGFloat?
 	@ObservedObject var store: Store<JourneyState, JourneyAction>
 	public init(_ store: Store<JourneyState, JourneyAction>) {
@@ -63,7 +52,7 @@ public struct JourneyContainerView: View {
 				.padding(0)
 				.frame(height: self.calendarHeight)
 			FilterPicker()
-			JourneyList(journeys: journeys)
+			JourneyList(journeys: store.value.filteredJourneys)
 			Spacer()
 		}
 	}
--- pabau2/src/Feature/Journey/JourneyState.swift
+++ pabau2/src/Feature/Journey/JourneyState.swift
@@ -1,10 +1,22 @@
 import Model
 import Util
+import NonEmpty
+import SwiftDate
 
 public struct JourneyState {
 	public init () {}
 	var loadingState: LoadingState<[Journey], RequestError> = .initial
-	var journeys: Set<Journey> = Set.init()
+	var journeys: Set<Journey> = [
+		Journey.init(id: 0,
+								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date(), to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#9400D3"))),
+								 patient: BaseClient.init(id: 0, firstName: "Andrej", lastName: "Trajkovski", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "Bojan Trajkovskiiii"), forms: [], photos: [], postCare: [], paid: "Not Paid"),
+		Journey.init(id: 1,
+								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date() + 1.days, to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Not Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#ec75ff"))),
+								 patient: BaseClient.init(id: 1, firstName: "Elon", lastName: "Musk", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "John Doe"), forms: [], photos: [], postCare: [], paid: "Paid"),
+		Journey.init(id: 2,
+								 appointments: NonEmpty.init(Appointment.init(id: 1, from: Date() + 1.days, to: Date(), employeeId: 1, locationId: 1, status: AppointmentStatus(id: 1, name: "Not Checked In"), service: BaseService.init(id: 1, name: "Botox", color: "#88fa69"))),
+								 patient: BaseClient.init(id: 2, firstName: "Joe", lastName: "Rogan", dOB: "28.02.1991", email: "andrej.", avatar: "emily", phone: ""), employee: Employee.init(id: 1, name: "Tiger Woods"), forms: [], photos: [], postCare: [], paid: "Owes 1.000")
+	]
 	var selectedFilter: CompleteFilter = .all
 	var selectedDate: Date = Date()
 	var selectedEmployees: [Employee] = []
@@ -12,4 +24,8 @@ public struct JourneyState {
 	var searchText: String = ""
 	var isShowingAddAppointment: Bool = false
 	var isShowingEmployees: Bool = false
+
+	var filteredJourneys: [Journey] {
+		return self.journeys.filter { $0.appointments.first.from.isInside(date: selectedDate, granularity: .day) }
+	}
 }
