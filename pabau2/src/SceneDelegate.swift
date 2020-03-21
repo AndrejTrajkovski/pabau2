e4704fde7147ffe5d885d857f8bb4277aa45f6ae
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-03-22 01:43:27 +0700

    setting logged in mock

--- pabau2/src/SceneDelegate.swift
+++ pabau2/src/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         rootView: ContentView(
           store: Store(
 						initialValue: AppState(
-							navigation: .login([.walkthroughScreen])
+							navigation: .tabBar(.journey)
 						),
 						reducer: reducer,
 						environment: AppEnvironment(
