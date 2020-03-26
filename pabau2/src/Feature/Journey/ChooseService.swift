c39777f8212e573ac61d40e8cee039463182f130
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-03-26 17:39:17 +0700

    refactoring choose service

--- pabau2/src/Feature/Journey/ChooseService.swift
+++ pabau2/src/Feature/Journey/ChooseService.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 import Model
 import ComposableArchitecture
-
+import Util
 func chooseServiceReducer(state: inout ChooseServiceState,
 													action: ChooseServiceAction,
 													environment: JourneyEnvironemnt) -> [Effect<ChooseServiceAction>] {
@@ -41,14 +41,28 @@ public struct ChooseServiceState: Equatable {
 	}
 
 	let services: [Service] = [
-		Service.init(id: 0, name: "Service 1", color: "#eb4034", categoryId: 1, categoryName: "Injectables"),
+		Service.init(id: 0, name: "Service 1", color: "#eb4034", categoryId: 1, categoryName: "Injectables", duration: "00:45"),
 		Service.init(id: 1, name: "Service 2", color: "#34eba5", categoryId: 2, categoryName: "Mosaic"),
-		Service.init(id: 2, name: "Service 3", color: "#34eba5", categoryId: 2, categoryName: "Mosaic"),
-		Service.init(id: 3, name: "Service 4", color: "#34eba5", categoryId: 2, categoryName: "Mosaic"),
+		Service.init(id: 2, name: "Service 3", color: "#34eba5", categoryId: 2, categoryName: "Mosaic", duration: "00:30"),
+		Service.init(id: 3, name: "Service 4", color: "#34eba5", categoryId: 2, categoryName: "Mosaic", duration: "00:45"),
 		Service.init(id: 4, name: "Service 5", color: "#eb34b1", categoryId: 3, categoryName: "Urethra"),
-		Service.init(id: 5, name: "Service 6", color: "#eb34b1", categoryId: 3, categoryName: "Urethra"),
-		Service.init(id: 6, name: "Service 7", color: "#eb34b1", categoryId: 3, categoryName: "Urethra")
+		Service.init(id: 5, name: "Service 6", color: "#eb34b1", categoryId: 3, categoryName: "Urethra", duration: "00:30"),
+		Service.init(id: 6, name: "MOS- Scar", color: "#FEC87C", categoryId: 4, categoryName: "MOS", duration: "00:30"),
+		Service.init(id: 7, name: "MOS- Acne Scar", color: "#FEC87C", categoryId: 4, categoryName: "MOS", duration: "00:30"),
+		Service.init(id: 8, name: "MOS- Skin Tightening", color: "#FEC87C", categoryId: 4, categoryName: "MOS", duration: "00:30"),
+		Service.init(id: 9, name: "MOS- Stretch Marks", color: "#FEC87C", categoryId: 4, categoryName: "MOS", duration: "00:30")
 	]
+	
+	var groupedServices: [Int : [Service]] {
+		 return [Int: [Service]].init(grouping: services, by: { $0.categoryId })
+	}
+	
+	var listServices: [[Service]] {
+		let res = self.groupedServices.map({ $0.value })
+			.sorted(by: { $0.first!.categoryId > $1.first!.categoryId})
+		print(res)
+		return res
+	}
 }
 
 public enum ChooseServiceAction {
@@ -57,21 +71,22 @@ public enum ChooseServiceAction {
 	case didTapBackBtn
 }
 
+struct FillAll: View {
+    let color: Color
+    var body: some View {
+        GeometryReader { proxy in
+            self.color.frame(width: proxy.size.width * 1.3).fixedSize()
+        }
+    }
+}
+
 struct ChooseService: View {
 	let store: Store<ChooseServiceState, ChooseServiceAction>
-	@ObservedObject var viewStore: ViewStore<ViewState>
+	@ObservedObject var viewStore: ViewStore<ChooseServiceState>
 	init (store: Store<ChooseServiceState, ChooseServiceAction>) {
 		self.store = store
-		self.viewStore = self.store
-			.scope(value: ViewState.init(state:), action: { $0 })
-			.view
-	}
-
-	struct ViewState: Equatable {
-		var groupedServices: [Int: [Service]]
-		var listServices: [[Service]] {
-			self.groupedServices.map({ $0.value })
-		}
+		self.viewStore = self.store.view
+		UITableViewHeaderFooterView.appearance().tintColor = UIColor.clear
 	}
 
 	@State var searchText: String = ""
@@ -82,15 +97,22 @@ struct ChooseService: View {
 				StaffFilterPicker()
 			}
 			List {
-				ForEach(self.viewStore.value.listServices, id: \.self.first!.categoryId) { (group: [Service]) in
-					Section(header: Text(group.first!.categoryName)) {
-						ForEach(group, id: \.self) { (item: Service) in
-							Text(item.name)
+				ForEach(self.viewStore.value.listServices, id: \.self.first?.categoryId) { (group: [Service]) in
+					Section(header:
+						ServicesHeader(name: group.first?.categoryName ?? "No name")
+					) {
+						ForEach(group, id: \.self) { (service: Service) in
+							ServiceRow(service: service)
 						}
-					}
+					}.background(Color.white)
 				}
 			}
-		}.padding()
+			Spacer()
+		}
+		.padding()
+		.navigationBarTitle("Services")
+		.customBackButton(action: { self.store.send(.didTapBackBtn)})
+		
 	}
 }
 
@@ -107,8 +129,27 @@ struct StaffFilterPicker: View {
 	}
 }
 
-extension ChooseService.ViewState {
-  init(state: ChooseServiceState) {
-		self.groupedServices = [Int: [Service]].init(grouping: state.services, by: { $0.categoryId })
+struct ServiceRow: View {
+	let service: Service
+	var body: some View {
+		HStack {
+			Circle()
+				.fill(Color.init(hex: service.color))
+				.frame(width: 22.0, height: 22.0)
+			Text(service.name).font(.regular17)
+			Spacer()
+			Text(service.duration ?? "Not set").font(.regular17)
+		}
+	}
+}
+
+struct ServicesHeader: View {
+	let name: String
+	var body: some View {
+		VStack(alignment: .leading, spacing: 16) {
+			Spacer()
+			Text(name).frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+			Divider()
+		}
 	}
 }
