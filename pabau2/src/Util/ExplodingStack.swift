bb3ff933064a17ab2ded0fcfa23a22c54b95a3e7
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-05-07 16:16:29 +0700

    adding exploding stack

---
+++ pabau2/src/Util/ExplodingStack.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+public extension View {
+	func exploding(_ alignment: Alignment) -> some View {
+		self.modifier(ExplodingElement(alignment: alignment))
+	}
+}
+
+struct ExplodingElement: ViewModifier {
+	let alignment: Alignment
+	func body(content: Content) -> some View {
+		ExplodingFrame({content}, alignment)
+	}
+}
+
+struct ExplodingFrame<Content: View>: View {
+	let content: () -> Content
+	let alignment: Alignment
+	init(@ViewBuilder _ content: @escaping () -> Content,
+										_ alignment: Alignment) {
+		self.content = content
+		self.alignment = alignment
+	}
+	
+	var body: some View {
+		content().frame(minWidth: 0, maxWidth: .infinity,
+										minHeight: 0, maxHeight: .infinity,
+										alignment: alignment)
+	}
+}
