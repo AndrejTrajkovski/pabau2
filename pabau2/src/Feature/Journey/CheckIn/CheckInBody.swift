6bff498ef0b0539f7acd943969e2081c55d9993b
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-05-29 00:27:33 +0700

    adding files

---
+++ pabau2/src/Feature/Journey/CheckIn/CheckInBody.swift
@@ -0,0 +1,80 @@
+import SwiftUI
+import Model
+import ComposableArchitecture
+import Util
+
+public enum CheckInBodyAction {
+	case toPatientMode
+	case updateForm(Indexed<UpdateFormAction>)
+	case didSelectCompleteFormIdx(Int)
+	case stepsView(StepsViewAction)
+}
+
+let checkInBodyReducer: Reducer<CheckInViewState, CheckInBodyAction, JourneyEnvironment> =
+	(
+	.combine(
+		.init { state, action, _ in
+			switch action {
+			case .updateForm:
+				break
+			case .didSelectCompleteFormIdx(let idx):
+				state.forms[idx].isComplete = true
+				goToNextStep(&state.stepsViewState)
+			case .toPatientMode:
+				break//handled in navigationReducer
+			case .stepsView:
+				break
+			}
+			return .none
+		},
+		stepsViewReducer.pullback(
+			state: \.stepsViewState,
+			action: /CheckInBodyAction.stepsView,
+			environment: { $0 })
+		)
+	)
+
+struct CheckInBody: View {
+
+	@EnvironmentObject var keyboardHandler: KeyboardFollower
+	let store: Store<CheckInViewState, CheckInBodyAction>
+	@ObservedObject var viewStore: ViewStore<CheckInViewState, CheckInBodyAction>
+
+	init(store: Store<CheckInViewState, CheckInBodyAction>) {
+		print("check in body init")
+		self.store = store
+		self.viewStore = ViewStore(store)
+	}
+
+	var body: some View {
+		print("check in main body")
+		return GeometryReader { geo in
+			VStack(spacing: 8) {
+				StepsCollectionView(store:
+					self.store.scope(
+						state: { $0.stepsViewState}, action: { .stepsView($0) })
+				).frame(height: 80)
+				Divider()
+					.frame(width: geo.size.width)
+					.shadow(color: Color(hex: "C1C1C1"), radius: 4, y: 2)
+				IfLetStore(self.store
+					.scope(state: { $0.selectedForm?.form },
+								 action: { .updateForm(Indexed(self.viewStore.state.selectedIndex, $0))
+					}), then: FormWrapper.init(store:))
+					.padding(.bottom,
+									 self.keyboardHandler.keyboardHeight > 0 ? self.keyboardHandler.keyboardHeight : 32)
+					.padding([.leading, .trailing, .top], 32)
+				Spacer()
+				if self.keyboardHandler.keyboardHeight == 0 &&
+					!self.viewStore.state.isOnCompleteStep {
+					FooterButtons(store: self.store.scope(
+						state: { $0 }, action: { $0 }
+					))
+					.frame(maxWidth: 500)
+					.padding(8)
+				}
+			}	.padding(.leading, 40)
+				.padding(.trailing, 40)
+		}
+	}
+}
---
+++ pabau2/src/Feature/Journey/Form/FormWrapper.swift
@@ -0,0 +1,139 @@
+import Model
+import SwiftUI
+import ComposableArchitecture
+import Util
+
+public typealias Indexed<T> = (Int, T)
+
+let metaFormAndStatusReducer: Reducer<MetaFormAndStatus, UpdateFormAction, JourneyEnvironment> =
+	metaFormReducer.pullback(
+		state: \MetaFormAndStatus.form,
+		action: /UpdateFormAction.self,
+		environment: { $0 }
+)
+
+let metaFormReducer: Reducer<MetaForm, UpdateFormAction, JourneyEnvironment> =
+	Reducer.combine(
+		Reducer.init { state, action, _ in
+			switch action {
+			//FIXME: GO WITH REDUX FOR TEMPLATES
+			case .didUpdateTemplate(let template):
+				state = MetaForm.init(template)
+			default:
+				break
+			}
+			return .none
+		},
+		patientCompleteReducer.pullbackCp(
+			state: /MetaForm.patientComplete,
+			action: /UpdateFormAction.patientComplete,
+			environment: { $0 }),
+		patientDetailsReducer.pullbackCp(
+			state: /MetaForm.patientDetails,
+			action: /UpdateFormAction.patientDetails,
+			environment: { $0 }),
+		aftercareReducer.pullbackCp(
+			state: /MetaForm.aftercare,
+			action: /UpdateFormAction.aftercare,
+			environment: { $0 })
+)
+
+public enum UpdateFormAction {
+	case patientComplete(PatientCompleteAction)
+	case didUpdateTemplate(FormTemplate)
+	case patientDetails(PatientDetailsAction)
+	case aftercare(AftercareAction)
+}
+
+struct FormWrapper: View {
+	let store: Store<MetaForm, UpdateFormAction>
+	@ObservedObject var viewStore: ViewStore<State, UpdateFormAction>
+
+	init(store: Store<MetaForm, UpdateFormAction>) {
+		self.store = store
+		self.viewStore = ViewStore(store.scope(
+			state: State.init(state:),
+			action: { $0 }))
+	}
+
+	struct State: Equatable {
+		var patientDetails: PatientDetails?
+		var template: FormTemplate?
+		var aftercare: Aftercare?
+		var patientCompleteForm: PatientComplete?
+		var checkPatient: CheckPatient?
+
+		init (state: MetaForm) {
+			self.patientDetails = extract(case: MetaForm.patientDetails, from: state)
+			self.template = extract(case: MetaForm.template, from: state)
+			self.aftercare = extract(case: MetaForm.aftercare, from: state)
+			self.patientCompleteForm = extract(case: MetaForm.patientComplete, from: state)
+			self.checkPatient = extract(case: MetaForm.checkPatient, from: state)
+		}
+	}
+
+	var body: some View {
+		if self.viewStore.state.template != nil {
+			return AnyView(
+				ListDynamicForm(template:
+					Binding.init(
+						get: { self.viewStore.state.template ?? FormTemplate.defaultEmpty },
+						set: { self.viewStore.send(.didUpdateTemplate($0)) })
+				)
+			)
+		} else if self.viewStore.state.patientDetails != nil {
+			return AnyView(
+				IfLetStore(
+					self.store.scope(
+						state: { extract(case: MetaForm.patientDetails, from: $0) },
+						action: { .patientDetails($0) }),
+					then: PatientDetailsForm.init(store:)
+				)
+			)
+		} else if self.viewStore.state.patientCompleteForm != nil {
+			return AnyView(
+				IfLetStore(
+					self.store.scope(
+						state: { extract(case: MetaForm.patientComplete, from: $0) },
+						action: { .patientComplete($0) }),
+					then: PatientCompleteForm.init(store:)
+				)
+			)
+		} else if self.viewStore.state.checkPatient != nil {
+			return AnyView(
+				CheckPatientForm(didTouchDone: { },
+												 patDetails: self.viewStore.state.checkPatient!.patDetails,
+												 patientForms: self.viewStore.state.checkPatient!.patForms)
+			)
+		} else {
+			return AnyView(
+				IfLetStore(
+					self.store.scope(
+						state: { extract(case: MetaForm.aftercare, from: $0) },
+						action: { .aftercare($0) }),
+					then: AftercareForm.init(store:)
+				)
+			)
+		}
+	}
+}
+//
+//self.store = store
+//self.journeyMode = journeyMode
+//self.viewStore = ViewStore(store.scope(
+//	state: {
+//		let forms = getForms(journeyMode, $0)
+//		if forms.count > selectedFormIndex {
+//			return State.init(state: forms[selectedFormIndex])
+//		} else {
+//			return State.defaultEmpty
+//		}
+//},
+//	action: {
+//		switch journeyMode {
+//		case .patient:
+//			return .patient(.stepForms(.childForm(Indexed(selectedFormIndex, $0))))
+//		case .doctor:
+//			return .doctor(.stepForms(.childForm(Indexed(selectedFormIndex, $0))))
+//		}
+//}))
