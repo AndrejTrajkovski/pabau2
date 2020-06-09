91b2595585a4aa060b06e38f66293914ac833d33
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-06-09 16:51:49 +0700

    adding PabauCommon

---
+++ PabauCommon/PabauCommon.h
@@ -0,0 +1,13 @@
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PabauCommon.
+FOUNDATION_EXPORT double PabauCommonVersionNumber;
+
+//! Project version string for PabauCommon.
+FOUNDATION_EXPORT const unsigned char PabauCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PabauCommon/PublicHeader.h>
+
+
