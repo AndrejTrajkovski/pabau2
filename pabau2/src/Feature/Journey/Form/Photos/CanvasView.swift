33ba05957da1bf95ff54f0527ad3b2e84bd3119d
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-06-13 21:52:12 +0700

    Adding CanvasView

---
+++ pabau2/src/Feature/Journey/Form/Photos/CanvasView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import PencilKit
+
+struct CanvasView: UIViewRepresentable {
+	
+	@Binding var drawing: PKDrawing
+	
+	func makeUIView(context: UIViewRepresentableContext<CanvasView>) -> PKCanvasView {
+		let canvasView = PKCanvasView()
+		if let window = UIApplication.shared.windows.filter({$0.isKeyWindow}).first,
+			let toolPicker = PKToolPicker.shared(for: window) {
+			toolPicker.addObserver(canvasView)
+			toolPicker.addObserver(context.coordinator)
+			toolPicker.setVisible(true, forFirstResponder: canvasView)
+		}
+		canvasView.becomeFirstResponder()
+		return canvasView
+	}
+	
+	func updateUIView(_ canvasView: PKCanvasView, context: UIViewRepresentableContext<CanvasView>) {
+		canvasView.drawing = drawing
+	}
+
+	/// Cleans up the presented `UIView` (and coordinator) in
+	/// anticipation of their removal.
+	static func dismantleUIView(_ uiView: PKCanvasView, coordinator: Coordinator) {
+	}
+	
+	public func makeCoordinator() -> Coordinator {
+		return Coordinator(self)
+	}
+
+	public class Coordinator: NSObject {
+		var parent: CanvasView
+
+		init(_ parent: CanvasView) {
+			self.parent = parent
+		}
+	}
+}
+
+extension CanvasView.Coordinator: PKCanvasViewDelegate {
+	public func canvasViewDrawingDidChange(_ canvasView: PKCanvasView) {
+		self.parent.drawing = canvasView.drawing
+	}
+}
+
+extension CanvasView.Coordinator: PKToolPickerObserver {
+	
+}
