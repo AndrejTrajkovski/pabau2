caba4ee795844b069e055e1dba338480a75182cb
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-07-21 02:43:22 +0700

    fixing pager reload

--- pabau2/src/Feature/Journey/CheckIn/FormPager.swift
+++ pabau2/src/Feature/Journey/CheckIn/FormPager.swift
@@ -4,30 +4,38 @@ import ComposableArchitecture
 struct FormPager: View {
 	
 	let store: Store<CheckInViewState, CheckInBodyAction>
-	@ObservedObject var viewStore: ViewStore<CheckInViewState, CheckInBodyAction>
+	@ObservedObject var viewStore: ViewStore<State, CheckInBodyAction>
 	init(store: Store<CheckInViewState, CheckInBodyAction>) {
 		self.store = store
-		self.viewStore = ViewStore(store)
+		self.viewStore = ViewStore(store.scope(state: State.init(state:)))
 	}
 
+	struct State: Equatable {
+		let formCount: Int
+		let selectedIndex: Int
+		init(state: CheckInViewState) {
+			self.formCount = state.forms.count
+			self.selectedIndex = state.selectedIndex
+		}
+	}
+	
 	var body: some View {
-		PagerView(pageCount: self.viewStore.forms.count,
+		PagerView(pageCount: self.viewStore.state.formCount,
 							currentIndex:
-			Binding.init(
-				get: { return self.viewStore.selectedIndex },
-				set: { idx in
-					self.viewStore.send(.stepsView(.didSelectFormIndex(idx)))
-			}),
+			self.viewStore.binding(
+				get: { $0.selectedIndex },
+				send: { .stepsView(.didSelectFormIndex($0)) }
+			),
 							content: {
 								ForEachStore(
 									self.store.scope(
 										state: { $0.forms },
 										action: CheckInBodyAction.updateForm(index:action:)),
 									id: \.form.title) { (childStore: Store<MetaFormAndStatus, UpdateFormAction>) in
-											FormWrapper.init(store: childStore.scope(state: { $0.form }))
-												.padding([.leading, .trailing], 32)
+										FormWrapper.init(store: childStore.scope(state: { $0.form }))
+											.padding([.leading, .trailing], 32)
 								}
-			}
+		}
 		)
 	}
 }
