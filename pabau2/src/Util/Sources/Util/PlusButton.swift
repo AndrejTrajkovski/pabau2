39ff45f5e2f496c1f0b147164c345c698e24b054
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-08-18 11:00:41 +0200

    adding plust button

---
+++ pabau2/src/Util/Sources/Util/PlusButton.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+public struct PlusButton: View {
+	let action: () -> Void
+	
+	public init (_ action: @escaping () -> Void) {
+		self.action = action
+	}
+	
+	public var body: some View {
+		Button(action: action, label: {
+			Image(systemName: "plus")
+				.font(.system(size: 20))
+				.frame(width: 44, height: 44)
+		})
+	}
+}
