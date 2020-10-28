5fa4d8c0c3b8b315865cbac7da379f19a4ebe13a
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-10-28 14:54:28 +0100

    anyhashablekeypath impl

--- pabau2/src/Calendar/Sources/Calendar/AnyHashableKeyPath.swift
+++ pabau2/src/Calendar/Sources/Calendar/AnyHashableKeyPath.swift
@@ -1,1 +1,14 @@
-
+public struct AnyHashableKeyPath<T> {
+	let get: (T) -> AnyHashable
+	let set: (inout T, AnyHashable) -> ()
+	
+	init<S: Hashable>(_ kpth: WritableKeyPath<T, S>) {
+		set = {
+			$0[keyPath: kpth] = $1.base as! S
+		}
+		
+		get = {
+			$0[keyPath: kpth]
+		}
+	}
+}
