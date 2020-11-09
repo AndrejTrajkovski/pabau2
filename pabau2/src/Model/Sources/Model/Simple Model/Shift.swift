8995a02d37d5b112d0682a1627a4d832766b8c44
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-11-09 17:44:06 +0100

    adding dummy shifts

--- pabau2/src/Model/Sources/Model/Simple Model/Shift.swift
+++ pabau2/src/Model/Sources/Model/Simple Model/Shift.swift
@@ -74,9 +74,11 @@ extension Shift {
 	public static func mock () -> [Date: [Location.ID: [Employee.Id: [Shift]]]] {
 		var shifts = [Shift]()
 		for (idx, emp) in Employee.mockEmployees.enumerated() {
-			let mockStartEnd = Date.mockStartAndEndDate(endRangeMax: 600)
-			let startOfDay = Calendar.init(identifier: .gregorian).startOfDay(for: mockStartEnd.0)
-			shifts.append(Shift.init(id: idx, employeeId: emp.id, locationId: emp.locationId, date: startOfDay, startTime: mockStartEnd.0, endTime: mockStartEnd.1))
+			Array(0...3).forEach { _ in
+				let mockStartEnd = Date.mockStartAndEndDate(endRangeMax: 600)
+				let startOfDay = Calendar.init(identifier: .gregorian).startOfDay(for: mockStartEnd.0)
+				shifts.append(Shift.init(id: idx, employeeId: emp.id, locationId: emp.locationId, date: startOfDay, startTime: mockStartEnd.0, endTime: mockStartEnd.1))
+			}
 		}
 		let byDate = Dictionary.init(grouping: shifts, by: { $0.date })
 		return byDate.mapValues { events in
