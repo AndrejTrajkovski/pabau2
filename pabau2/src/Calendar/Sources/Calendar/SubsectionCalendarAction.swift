f8ebeade57e49a929c93d4bb184e2f3e39dfacb7
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2020-11-10 21:01:16 +0100

    adding files

---
+++ pabau2/src/Calendar/Sources/Calendar/SubsectionCalendarAction.swift
@@ -0,0 +1,11 @@
+import Model
+import Foundation
+
+public enum SubsectionCalendarAction<Subsection: Identifiable & Equatable> {
+	case addAppointment(startDate: Date,
+						durationMins: Int,
+						dropKeys:(date: Date, location: Location.ID, subsection: Subsection.ID))
+	case editAppointment(startDate: Date,
+						 startKeys:(page: Int, withinPage: Int),
+						 dropKeys:(date: Date, location: Location.ID, subsection: Subsection.ID))
+}
