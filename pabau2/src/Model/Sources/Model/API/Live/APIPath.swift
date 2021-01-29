99731c04783313ad22d3bb3f2acb5b63b3ad9362
Yuriy B <yberdnikov@me.com>
2021-01-29 14:59:02 +0200

    Updated clients API path

--- pabau2/src/Model/Sources/Model/API/Live/APIPath.swift
+++ pabau2/src/Model/Sources/Model/API/Live/APIPath.swift
@@ -1,14 +1,14 @@
 public enum APIPath: String {
 	//Login
 	case sendConfirmation = "sendc"
-	case login = "/OAuth2/staff/login-check.php"
+	case login = "OAuth2/staff/login-check.php"
 	case resetPass = "reset"
 	//Journey
 	case getJourneys
 	//Calendar
 	
 	//Contacts/ Clients
-	case getClients
+	case getClients = "OAuth2/clients/get_clients.php"
 
     //Services
     case getServices
