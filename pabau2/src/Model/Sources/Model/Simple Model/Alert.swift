2ab621142171370c683c0fb171c5be6fa9122c53
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2021-02-08 16:12:20 +0100

    removing mock Alerts

--- pabau2/src/Model/Sources/Model/Simple Model/Alert.swift
+++ pabau2/src/Model/Sources/Model/Simple Model/Alert.swift
@@ -61,38 +61,4 @@ public struct Alert: Codable, Identifiable, Equatable {
         }
         
     }
-
-}
-
-extension Alert {
-	static let mockAlerts =
-	[
-		Alert(id: 1, title: """
-m Ipsum is simply dummy text of the printing and typesetting
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-industry. Lorem Ipsum has been the industry's standard dummy text
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-ever since the 1500s, when an unknown printer took a galley of type
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-and scrambled it to make a type specimen book. It has survived not
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-only five centuries, but also the leap into electronic typesetting,
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-remaining essentially unchanged. It was popularised in the 1960s with
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-the release of Letraset sheets containing Lorem Ipsum passages, and
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: """
-more recently with desktop publishing software like Aldus PageMaker
-including versions of Lorem Ipsum.
-""", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: "Omg!", date: Date(), ownerId: 1, status: "Enable", critical: false),
-		Alert(id: 1, title: "Omg prelive is down", date: Date(), ownerId: 1, status: "Enable", critical: false),
-	]
 }
