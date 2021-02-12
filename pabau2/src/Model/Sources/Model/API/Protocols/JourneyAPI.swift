0a9e32a24d153ea545b3a7369bdd74a878a43c0f
Yuriy B <yberdnikov@me.com>
2021-02-12 17:46:16 +0200

    Added createShift API method implememtation

--- pabau2/src/Model/Sources/Model/API/Protocols/JourneyAPI.swift
+++ pabau2/src/Model/Sources/Model/API/Protocols/JourneyAPI.swift
@@ -5,4 +5,5 @@ public protocol JourneyAPI {
 	func getTemplates(_ type: FormType) -> Effect<[FormTemplate], RequestError>
 	func getAppointments(startDate: Date, endDate: Date, locationIds: [Location.ID], employeesIds: [Employee.ID], roomIds: [Room.ID]) -> Effect<CalendarResponse, RequestError>
 	func getLocations() -> Effect<[Location], RequestError>
+    func createShift(shiftSheme: ShiftSchema) -> Effect<PlaceholdeResponse, RequestError>
 }
