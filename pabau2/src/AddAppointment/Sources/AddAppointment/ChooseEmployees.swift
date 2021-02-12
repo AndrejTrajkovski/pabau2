616d8b6583a8d21d3fe583a9fb99090568e410b4
Yuriy B <yberdnikov@me.com>
2021-02-12 17:43:41 +0200

    Moved ChooseEmployees view to ShareViews

--- pabau2/src/AddAppointment/Sources/AddAppointment/ChooseEmployees.swift
+++ pabau2/src/AddAppointment/Sources/AddAppointment/ChooseEmployees.swift
@@ -4,27 +4,6 @@ import ComposableArchitecture
 import Util
 import SharedComponents
 
-public struct ChooseEmployeesState: Equatable {
-    var isChooseEmployeesActive: Bool
-    var employees: IdentifiedArrayOf<Employee> = []
-    var filteredEmployees: IdentifiedArrayOf<Employee> = []
-    var chosenEmployee: Employee?
-    var searchText: String = "" {
-        didSet {
-            isSearching = !searchText.isEmpty
-        }
-    }
-    var isSearching = false
-}
-
-public enum ChooseEmployeesAction: Equatable {
-    case onAppear
-    case gotEmployeeResponse(Result<[Employee], RequestError>)
-    case didSelectEmployee(Employee)
-    case onSearch(String)
-    case didTapBackBtn
-}
-
 let chooseEmployeesReducer =
     Reducer<ChooseEmployeesState, ChooseEmployeesAction, AddAppointmentEnv> { state, action, env in
         switch action {
@@ -59,42 +38,3 @@ let chooseEmployeesReducer =
         }
         return .none
     }
-
-struct ChooseEmployeesView: View {
-    let store: Store<ChooseEmployeesState, ChooseEmployeesAction>
-    @ObservedObject var viewStore: ViewStore<ChooseEmployeesState, ChooseEmployeesAction>
-
-    init(store: Store<ChooseEmployeesState, ChooseEmployeesAction>) {
-        self.store = store
-        self.viewStore = ViewStore(store)
-        UITableViewHeaderFooterView.appearance().tintColor = UIColor.clear
-    }
-
-    var body: some View {
-        VStack {
-            SearchView(
-                placeholder: "Search",
-                text: viewStore.binding(
-                    get: \.searchText,
-                    send: ChooseEmployeesAction.onSearch)
-            )
-            List {
-                ForEach(self.viewStore.state.filteredEmployees, id: \.id) { employee in
-                    TextAndCheckMark(
-                        employee.name,
-                        employee.id == self.viewStore.state.chosenEmployee?.id
-                    ).onTapGesture {
-                        self.viewStore.send(.didSelectEmployee(employee))
-                    }
-                }
-            }
-        }
-        .onAppear {
-            self.viewStore.send(.onAppear)
-        }
-        .padding()
-        .navigationBarTitle("Employee")
-        .customBackButton(action: { self.viewStore.send(.didTapBackBtn)})
-    }
-}
-
