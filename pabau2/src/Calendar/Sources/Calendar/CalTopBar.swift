3cfebbf74b0e0523d5c3cab403890fd26c0fa4fd
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2021-05-20 12:40:44 +0200

    finishing filters

--- pabau2/src/Calendar/Sources/Calendar/CalTopBar.swift
+++ pabau2/src/Calendar/Sources/Calendar/CalTopBar.swift
@@ -37,19 +37,8 @@ struct CalTopBar: View {
 					}
 					Button(
 						action: { viewStore.send(.toggleFilters)},
-						label: {
-							switch viewStore.state.filtersLoadingState {
-							case .gotSuccess:
-								Text(Texts.filters)
-							case .gotError(_), .initial:
-								Image(systemName: "exclamationmark.triangle.fill")
-									.font(.system(size: 20))
-									.foregroundColor(.red)
-							case .loading:
-								ActivityIndicator(isAnimating: .constant(true), style: .medium)
-									.foregroundColor(Color.blue)
-							}
-						})
+						label: filtersLabel
+					)
 				}
 				.padding()
 				.padding(.trailing, 20)
@@ -71,6 +60,21 @@ struct CalTopBar: View {
 			PlusButton {
 				self.viewStore.send(.onAddShift)
 			}
+		}
+	}
+	
+	@ViewBuilder
+	func filtersLabel() -> some View {
+		switch viewStore.state.filtersLoadingState {
+		case .gotSuccess:
+			Text(Texts.filters)
+		case .gotError(_), .initial:
+			Image(systemName: "exclamationmark.triangle.fill")
+				.font(.system(size: 20))
+				.foregroundColor(.red)
+		case .loading:
+			ActivityIndicator(isAnimating: .constant(true), style: .medium)
+				.foregroundColor(Color.blue)
 		}
 	}
 }
