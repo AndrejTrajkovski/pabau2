09ef536c591d0d352da086f5ac53d9b4b08574b2
AndrejTrajkovski <andrej.trajkovski@hotmail.com>
2021-06-04 14:10:30 +0200

    parsing step entry

--- pabau2/src/Model/Sources/Model/Simple Model/Pathway.swift
+++ pabau2/src/Model/Sources/Model/Simple Model/Pathway.swift
@@ -1,11 +1,11 @@
 import Tagged
 
 public struct Pathway: Decodable, Identifiable, Equatable {
-
+	
 	public typealias ID = Tagged<Pathway, EitherStringOrInt>
 	
 	public let id: ID
-
+	
 	public let stepEntries: Dictionary<Step.Id, StepEntry>
 	
 	enum CodingKeys: String, CodingKey {
@@ -14,6 +14,7 @@ public struct Pathway: Decodable, Identifiable, Equatable {
 	}
 	
 	public init(from decoder: Decoder) throws {
+		
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		self.id = try container.decode(Self.ID.self, forKey: .id)
 		let stepEntries = try container.decode(Dictionary<String, StepEntry>.self, forKey: .stepEntries)
--- pabau2/src/Model/Sources/Model/Simple Model/StepEntry.swift
+++ pabau2/src/Model/Sources/Model/Simple Model/StepEntry.swift
@@ -1,6 +1,20 @@
 public struct StepEntry: Decodable, Equatable {
-	let form_template_name: String
-//	public let status: StepStatus
-//	public let formTemplateId: FormTemplateInfo.ID?
-//	public let formEntryId: FilledFormData.ID?
+	
+	let formTemplateName: String
+	public let formTemplateId: FormTemplateInfo.ID?
+	public let formEntryId: FilledFormData.ID?
+	
+	enum CodingKeys: String, CodingKey {
+		case formTemplateName = "form_template_name"
+		case formTemplateIds = "form_template_ids"
+		case formEntryId = "form_entry_id"
+	}
+	
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		self.formTemplateName = try container.decode(String.self, forKey: .formTemplateName)
+		let templateIdsArray = try container.decode([FormTemplateInfo.ID].self, forKey: .formTemplateIds)
+		self.formTemplateId = templateIdsArray.first
+		self.formEntryId = try container.decode(FilledFormData.ID.self, forKey: .formEntryId)
+	}
 }
