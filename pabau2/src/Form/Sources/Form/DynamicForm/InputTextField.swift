f375f15b8ffbd2448b2047bef4bf6e0376fdb35e
andrej <andrej@pabau.com>
2021-08-06 00:04:36 +0200

    reverting input text field

--- pabau2/src/Form/Sources/Form/DynamicForm/InputTextField.swift
+++ pabau2/src/Form/Sources/Form/DynamicForm/InputTextField.swift
@@ -37,36 +37,36 @@ let initDatePickerStore: (Store<Date?, DatePickerTCAAction>) -> DatePickerTCA = 
 }
 
 //This LAGS
-struct InputTextField: View {
-	let store: Store<String?, TextChangeAction>
-
-	var body: some View {
-		WithViewStore(store) { viewStore in
-			TextField("", text: viewStore.binding(get: { $0 ?? "" }, send: { .textChange($0) }))
-				.textFieldStyle(RoundedBorderTextFieldStyle())
-		}
-	}
-}
-
-//struct InputTextFieldWrapper: View {
-//	@State var myText: String
-//	var onChange: (String) -> Void
+//struct InputTextField: View {
+//	let store: Store<String?, TextChangeAction>
 //
-//	init(store: Store<String?, TextChangeAction>) {
-//		let viewStore = ViewStore(store)
-//        self._myText = State.init(initialValue: viewStore.state ?? "")
-//		self.onChange = { viewStore.send(.textChange($0)) }
-//	}
-////	init (initialValue: String, onChange: @escaping (String) -> Void) {
-////		self._myText = State.init(initialValue: initialValue)
-////		self.onChange = onChange
-////	}
 //	var body: some View {
-//		//https://stackoverflow.com/a/56551874/3050624
-//		TextField.init("", text: $myText, onEditingChanged: { _ in
-//			self.onChange(self.myText)
-//		}, onCommit: {
-//		})
-//		.textFieldStyle(RoundedBorderTextFieldStyle())
+//		WithViewStore(store) { viewStore in
+//			TextField("", text: viewStore.binding(get: { $0 ?? "" }, send: { .textChange($0) }))
+//				.textFieldStyle(RoundedBorderTextFieldStyle())
+//		}
 //	}
 //}
+
+struct InputTextField: View {
+	@State var myText: String
+	var onChange: (String) -> Void
+
+	init(store: Store<String?, TextChangeAction>) {
+		let viewStore = ViewStore(store)
+        self._myText = State.init(initialValue: viewStore.state ?? "")
+		self.onChange = { viewStore.send(.textChange($0)) }
+	}
+//	init (initialValue: String, onChange: @escaping (String) -> Void) {
+//		self._myText = State.init(initialValue: initialValue)
+//		self.onChange = onChange
+//	}
+	var body: some View {
+		//https://stackoverflow.com/a/56551874/3050624
+		TextField.init("", text: $myText, onEditingChanged: { _ in
+			self.onChange(self.myText)
+		}, onCommit: {
+		})
+		.textFieldStyle(RoundedBorderTextFieldStyle())
+	}
+}
